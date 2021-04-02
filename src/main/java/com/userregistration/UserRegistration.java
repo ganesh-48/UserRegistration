c0631bf6a4d67db66102a517737dbc0bd288a94c
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-04-03 00:15:17 +0530

    ADDED: Welcome to User Registration Problem

---
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -0,0 +1,40 @@
+package com.userregistration;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    Scanner scanner = new Scanner(System.in);
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName(){
+        System.out.println("Enter your first name:");
+        firstName = scanner.nextLine();
+        return firstName;
+    }
+
+    /*@Description-*User enters a first name using regex
+    *it prints the name is valid or invalid
+     */
+    public boolean registerFirstName(String firstName) {
+        Pattern pattern = Pattern.compile("[A-Z]{1}[a-z]{2}+");
+        Matcher matcher = pattern.matcher(firstName);
+        boolean matches = matcher.find();
+        if(matches){
+            System.out.println("your first name is valid");
+        }else{
+            System.out.println("Your first name is invalid");
+        }
+        return matches;
+    }
+
+    public static void main(String[] args){
+        UserRegistration userRegistration = new UserRegistration();
+        String firstName = userRegistration.getFirstName();
+        userRegistration.registerFirstName(firstName);
+
+    }
+
+}
---
+++ src/test/java/com/userregistration/UserRegistrationDetails.java
@@ -0,0 +1,15 @@
+package com.userregistration;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistrationDetails {
+    UserRegistration userRegistration = new UserRegistration();
+    @Test
+    public void givenFirstName_whenProper_shouldReturnTrue() {
+        boolean result = userRegistration.registerFirstName("Ram");
+        Assert.assertEquals(result, true);
+    }
+
+
+}
