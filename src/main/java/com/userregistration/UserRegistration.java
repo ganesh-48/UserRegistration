1d3d365631d8dd17ae2849a9d4296d6214b2ca1c
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-04-03 22:41:38 +0530

    ADDED:Added At leat one numeric number in password

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -108,7 +108,8 @@ public class UserRegistration {
      *password should  havae at least one upper case character
       */
     public boolean registraterPassword(String password) {
-        Pattern pattern = Pattern.compile("(?=.*[A-Z])[0-9a-z$%&*@!#]{8,}$");
+        Pattern pattern = Pattern.compile("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\\\S+$).{8,}$",
+                Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(password);
         boolean matches = matcher.find();
         if(matches){
